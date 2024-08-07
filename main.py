b91281b7b6400b4a2c7c45ae5922dac1cf35a95c
gosleii <sleii@inbox.ru>
2024-08-07 22:14:25 +0300

    update

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ bot = telebot.TeleBot('7349729194:AAH83qC4D5Q_N3E-saSZ9ZSD_g_mKY5jzxw')
 
 @bot.message_handler(commands=['start'])
 def start_handler(message):
-    bot.send_message(message.chat.id, '*СПИСОК КОМАНД*\n /chanel -открытые каналы для подготовки ОГЭ/ЕГЭ \
-    \n /motivation - команда на случай, если тебе не хватает мотивации \
-    \n /lk_um - переход на сайт Умскул', parse_mode = 'Markdown')
+    bot.send_message(message.chat.id, '*СПИСОК КОМАНД* \n'
+                                      '/chanel - открытые каналы для подготовки ОГЭ/ЕГЭ \n'
+                                      '/motivation - команда на случай, если тебе не хватает мотивации \n '
+                                      '/lk-um - переход на сайт Умскул', parse_mode='Markdown')
+
 
 @bot.message_handler(commands=['chanel'])
 def chanel_handler(message):
@@ -16,7 +18,7 @@ def chanel_handler(message):
 def motivation_handler(message):
     bot.send_message(message.chat.id, 'У тебя все получится! Я в тебя верю!', parse_mode = 'Markdown')
 
-@bot.message_handler(commands=['lk_um'])
+@bot.message_handler(commands=['lk-um'])
 def lk_um_handler(message):
     bot.send_message(message.chat.id, '_Нажми, чтобы перейти на сайт_ -> [тык](https://umschool.net/) ', parse_mode = 'Markdown')
 
